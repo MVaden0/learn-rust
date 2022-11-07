9fe907727ed537a4911bfa2c572e1a285cc6cad1
Michael Vaden <michaelvaden03@gmail.com>
2022-11-07 10:40:40 -0500

    finished learning about cargo

---
+++ hello-cargo/target/debug/deps/hello_cargo-a232cad3996cd9c4.d
@@ -0,0 +1,5 @@
+/home/michael/code/learn-rust/hello-cargo/target/debug/deps/hello_cargo-a232cad3996cd9c4: src/main.rs
+
+/home/michael/code/learn-rust/hello-cargo/target/debug/deps/hello_cargo-a232cad3996cd9c4.d: src/main.rs
+
+src/main.rs:
---
+++ hello-cargo/target/debug/hello-cargo.d
@@ -0,0 +1,1 @@
+/home/michael/code/learn-rust/hello-cargo/target/debug/hello-cargo: /home/michael/code/learn-rust/hello-cargo/src/main.rs

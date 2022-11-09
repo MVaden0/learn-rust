fcf326c90befebfb4af35ba2a5cdf582a5358441
Michael Vaden <michaelvaden03@gmail.com>
2022-11-09 10:31:18 -0500

    finished learning about data types in rust

---
+++ data-types/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // rust has similar scalar types to other languages
+    let int: u8 = 10;
+    let float: f32 = 32.345;
+    let boolean: bool = false;
+    let character: char = 'a';
+
+    // tuple
+    let tuple: (u8, f32) = (8, 34.02);
+
+    // array
+    let array = [1, 2, 3];
+}
